b92e2ef81a71e4435942ee5595ffdee343e85cbb
M.Chinmayee <mchinmayee21@gmail.com>
2026-03-29 11:49:42 +0530

    update API URL to Render

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const downloadPDF = () => {
 };
   const handlePredict = async () => {
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://alzheimer-s-disease-prediction-system.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
